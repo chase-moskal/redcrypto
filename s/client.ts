2c236c51360749db82aa83ae8cbf6e2df8e6a5b0
Chase Moskal <chasemoskal@gmail.com>
2023-07-07 06:50:58 -0700

    add: index.js and client.js re-exports
     - close #4

---
+++ s/client.ts
@@ -0,0 +1,6 @@
+
+export * from "./base64.js"
+export * from "./redcrypto-rejection-error.js"
+export * from "./token-decode.js"
+export * from "./types.js"
+
---
+++ s/index.ts
@@ -0,0 +1,13 @@
+
+export * from "./client.js"
+
+export * from "./curries/curry-sign-token.js"
+export * from "./curries/curry-verify-token.js"
+export * from "./curries/mock-sign-token.js"
+export * from "./curries/mock-verify-token.js"
+
+export * from "./signature-sign.js"
+export * from "./signature-verify.js"
+export * from "./token-sign.js"
+export * from "./token-verify.js"
+
